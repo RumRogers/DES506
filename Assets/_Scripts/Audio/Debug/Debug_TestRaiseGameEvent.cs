cab985c8ebdfa92670c6baa162bf224493f66bd9
Andrea Serreli <andreaserreli1984@gmail.com>
2020-06-03 16:03:58 +0200

    Added ExtendedStudioEventEmitter class and quick Observer Pattern implementation
    - class ExtendedStudioEventEmitter inherits from StudioEventEmitter
    - it contains an empty list of strings that audio programmers should populate with the event ids that should cause the audio to be played
    - as the game starts, ExtendedStudioEventEmitter subscribes to AudioEventsPublisher (the observable entity), which in turn will call play each time one of the specified game events is raised
    - also added a small debug script for testing event raising and reactions

---
+++ Assets/_Scripts/Audio/Debug/Debug_TestRaiseGameEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameAudio
+{
+    public class Debug_TestRaiseGameEvent : MonoBehaviour
+    {
+        string m_eventName = "";
+        [SerializeField]
+        Vector2 m_topLeft = new Vector2(10, 10);
+        Vector2 m_labelSize = new Vector2(100, 20);
+        Vector2 m_btnSize = new Vector2(100, 20);
+        float m_verticalSpacing = 5f;
+
+        private void OnGUI()
+        {
+            Vector2 pos = m_topLeft;
+
+            m_eventName = GUI.TextField(new Rect(pos.x, pos.y, m_labelSize.x, m_labelSize.y), m_eventName, 25);
+
+            pos.y += m_labelSize.y + m_verticalSpacing;
+
+            if (GUI.Button(new Rect(pos.x, pos.y, m_btnSize.x, m_btnSize.y), "Raise Event"))
+            {
+                AudioEventsPublisher.RaiseGameEvent(m_eventName);
+
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Audio/Editor/ExtendedStudioEventEmitterEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using UnityEditor;
+
+[CustomEditor(typeof(GameAudio.ExtendedStudioEventEmitter))]
+[CanEditMultipleObjects]
+public class ExtendedStudioEventEmitterEditor : FMODUnity.StudioEventEmitterEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var list = serializedObject.FindProperty("m_gameEvents");
+        EditorGUILayout.PropertyField(list, new GUIContent("Triggered by game events"), true);
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+}
