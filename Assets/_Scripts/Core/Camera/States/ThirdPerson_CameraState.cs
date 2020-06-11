ea8320214fd325ffd5bc309aca2bccf51db03971
JJRWalker <jjrwalkerdev@gmail.com>
2020-06-11 20:21:50 +0100

    Added player aiming state and third person camera states
    - Player can press e to enter aiming state that gives them direct control of the camera
    - Default camera state can be rotated but maintains the same pitch as previously (at request of design)
    - Added third person camera state for aiming that is transitioned to from the player aim state constructor
    - Added transition from this state back to the default camera state

---
+++ Assets/_Scripts/Core/Camera/States/ThirdPerson_CameraState.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameCore.Camera
+{
+    public class ThirdPerson_CameraState : GameCore.System.State
+    {
+        PlayerMoveCamera m_playerMoveCamera;
+        Vector3 m_rotation;
+        Vector3 m_offset;
+
+        bool m_transitioned = false;
+        Vector3 m_startingPos;
+        Vector3 m_endingPos;
+
+        public ThirdPerson_CameraState(GameCore.System.Automaton owner) : base (owner)
+        {
+            m_playerMoveCamera = (PlayerMoveCamera)owner;
+
+            m_rotation = m_playerMoveCamera.transform.eulerAngles;
+
+            //Getting initial start and offset
+            m_startingPos = m_playerMoveCamera.transform.position;
+            m_offset = (m_playerMoveCamera.transform.right * m_playerMoveCamera.p_AimingOffset.x) + (m_playerMoveCamera.transform.up * m_playerMoveCamera.p_AimingOffset.y);
+
+            //Camera transition between current position and expected distance without changing angle.
+            m_playerMoveCamera.StartCoroutine(Transition());
+        }
+
+        public override void Manage()
+        {
+            //Only movable after the transition
+            if (m_transitioned)
+            {
+                m_rotation.x = Mathf.Clamp(m_rotation.x - (Input.GetAxis("Mouse Y") * m_playerMoveCamera.p_AimingMovementSpeed), -m_playerMoveCamera.p_AimingMaxAngle, m_playerMoveCamera.p_AimingMaxAngle);
+                m_rotation.y += Input.GetAxis("Mouse X") * m_playerMoveCamera.p_AimingMovementSpeed;
+
+                m_playerMoveCamera.transform.eulerAngles = m_rotation;
+
+                m_offset = (m_playerMoveCamera.transform.right * m_playerMoveCamera.p_AimingOffset.x) + (m_playerMoveCamera.transform.up * m_playerMoveCamera.p_AimingOffset.y);
+
+                Vector3 targetPosition = m_playerMoveCamera.p_CameraTarget.position - ((m_playerMoveCamera.transform.forward * m_playerMoveCamera.p_AimingDistance) - m_offset);
+
+                m_playerMoveCamera.transform.position = targetPosition;
+
+            }
+        }
+
+        IEnumerator Transition()
+        {
+            float time = 0;
+            while (true)
+            {
+                m_offset = (m_playerMoveCamera.transform.right * m_playerMoveCamera.p_AimingOffset.x) + (m_playerMoveCamera.transform.up * m_playerMoveCamera.p_AimingOffset.y);    //updating offset as the player may be moving
+                m_endingPos = m_playerMoveCamera.p_CameraTarget.position - ((m_playerMoveCamera.transform.forward * m_playerMoveCamera.p_AimingDistance) - m_offset);
+                m_playerMoveCamera.transform.position = Vector3.Lerp(m_startingPos, m_endingPos, time);
+
+                time += Time.deltaTime * m_playerMoveCamera.p_ComebackSpeed;
+
+                if (time > 1)
+                {
+                    m_transitioned = true;
+                    yield break;
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
--- Assets/_Scripts/Player/PlayerStates/Aiming_PlayerState.cs
+++ Assets/_Scripts/Player/PlayerStates/Aiming_PlayerState.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 namespace Player
 {
-    public class Default_PlayerState : GameCore.System.State
+    public class Aiming_PlayerState : GameCore.System.State
     {
+
         PlayerEntity m_playerEntity;
         GameCore.Camera.PlayerMoveCamera m_camera;
+        Vector3 m_velocity;
 
-        Vector3 m_velocity;  //local velocity varable, easier to manipulate individual components, added to the players velocity at end of Manage()
-
-        public Default_PlayerState(GameCore.System.Automaton owner) : base(owner)
+        public Aiming_PlayerState(GameCore.System.Automaton owner) : base(owner)
         {
             m_playerEntity = (PlayerEntity)owner;
-            m_velocity = new Vector3(m_playerEntity.Velocity.x, 0.0f, m_playerEntity.Velocity.z);
-
+            m_velocity = m_playerEntity.Velocity;
             if (!Camera.main.transform.TryGetComponent<GameCore.Camera.PlayerMoveCamera>(out m_camera))
             {
                 Debug.LogError("Cannot get PlayerMoveCamera Component on Main Camera!");
             }
-            m_camera.SetState(new GameCore.Camera.Default_CameraState(m_camera));
+            m_camera.SetState(new GameCore.Camera.ThirdPerson_CameraState(m_camera));
             //Storing a reference to this state object to transition back to after a fall
             m_playerEntity.PreviousGroundState = this;
         }
 
         public override void Manage()
         {
-            //Jumping / falling state transitions
+
+            //if button "aim" up get out of this state, using the Interact button for now
+            if (Input.GetButtonDown("Interact"))
+            {
+                { 
+                    m_owner.SetState(new Default_PlayerState(m_owner));
+                    return;
+                }
+            }
+
             if (m_playerEntity.IsGrounded())
             {
                 //need to check if it's playable here before jump as we still want it to enter the falling state if it's not grounded, regardless of it's properties
@@ -42,34 +50,17 @@ namespace Player
                 m_owner.SetState(new Falling_PlayerState(m_owner));
                 return;
             }
+
+            //update movement relative to the camera
             if (m_playerEntity.HasProperty(PlayerEntityProperties.PLAYABLE))
             {
-                //Pushing state transition, gets closest interactable and switches state if there are interactables in range 
-                if (Input.GetButtonDown("Interact"))
-                {
-                    if (m_playerEntity.InteractablesInRange.Count > 0)
-                    {
-                        float closestDistance = (m_playerEntity.InteractablesInRange[0].position - m_playerEntity.transform.position).magnitude;
-                        m_playerEntity.ClosestInteractable = m_playerEntity.InteractablesInRange[0];
-                        foreach (Transform t in m_playerEntity.InteractablesInRange)
-                        {
-                            float distance = (t.position - m_playerEntity.transform.position).magnitude;
-                            if (distance < closestDistance)
-                            {
-                                m_playerEntity.ClosestInteractable = t;
-                                closestDistance = distance;
-                            }
-                        }
-                        //m_owner.SetState(new Pushing_PlayerState(m_owner));
-                    }
-                        m_owner.SetState(new Aiming_PlayerState(m_owner)); 
-                        return;
-                }
-
                 //Directional input
                 Vector3 forwardMovement = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z) * Input.GetAxis("Vertical"); // removing the y component from the camera's forward vector
                 Vector3 rightMovement = Camera.main.transform.right * Input.GetAxis("Horizontal");
                 m_playerEntity.Direction = (forwardMovement + rightMovement).normalized;
+
+                //rotate the player to face the direction they are aiming in
+                m_playerEntity.transform.rotation = Quaternion.LookRotation(new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z));
 
                 //Slope detection 
                 //if the slope is climable, modify the direction the player is traveling in 
@@ -78,7 +69,7 @@ namespace Player
                 {
                     Vector3 slopeDirection = Vector3.zero;
                     if (m_playerEntity.HasProperty(PlayerEntityProperties.SLIDING) && slopeAngle > 1)
-                    {                    
+                    {
                         //get the players right based on direction of movement, then use it to calculate the new direction of travel
                         Vector3 playerRight = Vector3.Cross(m_playerEntity.transform.forward, -m_playerEntity.transform.up);
                         //getting the slope angle for the ground the player is walking on
@@ -110,8 +101,6 @@ namespace Player
                 //If there is input, add velocity in that direction, but clamp the movement speed, Y velocity should always equal zero in this state, so no need to preserve down / up velocity
                 if (m_playerEntity.Direction != Vector3.zero)
                 {
-                    //rotate the player to face the direction they are traveling in
-                    m_playerEntity.transform.rotation = Quaternion.LookRotation(m_playerEntity.Direction);
                     if (m_playerEntity.HasProperty(PlayerEntityProperties.SLIDING))
                     {
                         m_velocity += (m_playerEntity.Direction * m_playerEntity.IceAcceleration) * Time.deltaTime;
@@ -151,6 +140,7 @@ namespace Player
             }
 
             m_playerEntity.Velocity = m_velocity;
+
         }
     }
 }
