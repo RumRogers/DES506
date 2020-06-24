0d1803fd9238005c56880ea1d37f6305977d4633
Alastair Carr <AliCarr@users.noreply.github.com>
2020-06-24 16:18:47 +0100

    NPC states created
    I've not got the simplified NPC working with it's state types. continued to be simplified.  It has a default state in which it rotates around to face the player provided they are in view. The second state moves the camera closer and over the shoulder, though this needs a little updating as I feel it is too close, and too sudden

--- Assets/_Scripts/AI (Simplified)/NPCSimple.cs
+++ Assets/_Scripts/AI (Simplified)/NPCSimple.cs
@@ -7,53 +7,77 @@ public class NPCSimple : MonoBehaviour
     [SerializeField]
     private GameObject m_player;
 
-    private bool m_isPlayerInRange = false;
-    private const float c_viewRadius = 5.0f;
-    Vector3 playerToTarget;
-    Vector3 test;
+    [SerializeField]
+    private string m_dialog;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private Camera m_playerCamera;
+
+    private bool m_isPlayerTalking = false;
+    private const float c_viewRadius = 5.0f;
+    private Vector3 c_offset = new Vector3(1, 1, 1);
+    private Vector3 playerToTarget;
+    private Vector3 test;
+
     void Start()
     {
-        
+        test = transform.forward;
+
+        if (!m_player || !m_playerCamera)
+            Debug.LogError(name + " is missing a component");
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        switch(m_isPlayerTalking)
+        {
+            case true:
+                PlayerInteractionState();
+                break;
+
+            case false:
+                DefaultState();
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+            m_isPlayerTalking = true;
+    }
+
+    private void PlayerInteractionState()
+    {
+        //Camera changes position
+        m_playerCamera.transform.position = m_player.transform.localPosition + c_offset;
+        m_playerCamera.transform.LookAt(transform);
+        //Letter box effect?
+        //NPC's dialog 
+    }
+
+    public void PlayerInteracts()
+    {
+        m_isPlayerTalking = true;
+    }
+
+    private void DefaultState()
     {
         playerToTarget = m_player.transform.position - this.transform.position;
         playerToTarget.y = 0;
 
         float angle = Vector3.Angle(transform.forward, playerToTarget);
-        Vector3 cross = Vector3.Cross(transform.forward, playerToTarget);
 
-        if (cross.y < 0)
-            angle = -angle;
-
-        Debug.Log(angle);
-        Quaternion target = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-
-
-
-        test = new Vector3(target.x, target.y, target.z);
-
-        test = Quaternion.Euler(0, angle, 0) * transform.forward;
-       
-        
         if (Vector3.Distance(m_player.transform.position, transform.position) <= c_viewRadius)
         {
-            test = Vector3.Lerp(transform.forward, test, 0.1f);
-            
-           // transform.LookAt(new Vector3(m_player.transform.position.x, transform.position.y, m_player.transform.position.z), m_player.transform.up);            
+            test = Vector3.RotateTowards(test, playerToTarget, Time.deltaTime * (angle / 60), 0.0f);
+            transform.rotation = Quaternion.LookRotation(test);
         }
     }
 
     private void OnDrawGizmosSelected()
     {
-        //Gizmos.color = Color.red;
-        //Gizmos.DrawRay(transform.position, transform.forward * 10);
-
         Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, playerToTarget * 10);
+
+        Gizmos.color = Color.blue;
         Gizmos.DrawRay(transform.position, test * 10);
     }
 }
