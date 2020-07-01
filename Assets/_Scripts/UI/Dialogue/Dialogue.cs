06732a9b456bae1fa8b2c162d4dac365a02acd8c
Jordan Walsh <jordanwalsh9519@gmail.com>
2020-07-01 17:10:39 +0100

    Loads of minor changes to menus

---
+++ Assets/_Scripts/UI/Dialogue/Dialogue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameUI.Dialogue
+{
+    public class Line //going to set this up this way so we can extend the line class to include additional info, i.e. Speaker, options, etc.
+    {
+        public string text;
+    }
+
+    public class Dialogue : MonoBehaviour
+    {
+        Transform m_player;
+        Transform m_talkingNPC;
+
+        [SerializeField]
+        TextAsset m_convoJSON;
+
+        Line[] m_lines;
+
+        private void Awake()
+        {
+            m_lines = JsonUtility.FromJson<Line[]>(m_convoJSON.text);
+        }
+
+    }
+}
