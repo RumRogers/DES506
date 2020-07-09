821a5908a78d3be4d9a96a5be88f6f7ce5f07125
DmitriiGruzdev <d1gruzdev@gmail.com>
2020-07-09 15:03:00 +0100

    SFX Animation manager and footstep triggers
    There is a new SFX manager script which I will be using to trigger the corresponding SFX from animations, I have done it so far with footsteps.

---
+++ Assets/_Scripts/Audio/AnimationSFXManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+
+public class AnimationSFXManager : MonoBehaviour
+{
+    public void playLeftstepSFX ()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerMovement/Player_LeftStep");
+    }
+
+    public void playRightstepSFX()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerMovement/Player_RightStep");
+    }
+
+    public void testingSFX()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/Button_Click");
+    }
+}
