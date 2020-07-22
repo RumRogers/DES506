2bb2b2fe59c2924f45da06cfa8678a04c318be7a
Alastair Carr <AliCarr@users.noreply.github.com>
2020-07-22 17:25:26 +0100

    Fixed some issues with the spell effect
    The origin point was acting weirdly, starting at a spot quite a few units off the object it's using as a reference. It now works, and has some extra stuff for better handling direction

--- Assets/SpellVFX/Package/Scripts/RFX4_EffectEvent.cs
+++ Assets/SpellVFX/Package/Scripts/RFX4_EffectEvent.cs
@@ -3,65 +3,108 @@ using System.Collections;
 
 public class RFX4_EffectEvent : MonoBehaviour
 {
-    public GameObject CharacterEffect;
-    public Transform CharacterAttachPoint;
-    public float CharacterEffect_DestroyTime = 10;
+    [Header("Quil Spell Effect")]
+    [SerializeField]
+    private GameObject CharacterEffect;
+
+    [SerializeField]
+    private Transform CharacterAttachPoint;
+
+    [SerializeField]
+    private float CharacterEffect_DestroyTime = 10;
     [Space]
 
-    public GameObject CharacterEffect2;
-    public Transform CharacterAttachPoint2;
-    public float CharacterEffect2_DestroyTime = 10;
-    [Space]
+    //public GameObject CharacterEffect2;
+    //public Transform CharacterAttachPoint2;
+    //public float CharacterEffect2_DestroyTime = 10;
+    //[Space]
 
-    public GameObject MainEffect;
-    public Transform AttachPoint;
-    public Transform OverrideAttachPointToTarget;
-    public float Effect_DestroyTime = 10;
-    [Space]
+    [Header("Main Spell Effect")]
+    [SerializeField]
+    private GameObject MainEffect;
 
-    public GameObject AdditionalEffect;
-    public Transform AdditionalEffectAttachPoint;
-    public float AdditionalEffect_DestroyTime = 10;
+    [SerializeField]
+    private Transform AttachPoint;
+
+    [SerializeField]
+    private Transform OverrideAttachPointToTarget;
+
+    [SerializeField]
+    private float Effect_DestroyTime = 10;
+
+    [SerializeField]
+    private float m_forwardOffset;
+    //public GameObject AdditionalEffect;
+    //public Transform AdditionalEffectAttachPoint;
+    //public float AdditionalEffect_DestroyTime = 10;
 
     [HideInInspector] public bool IsMobile;
+
+    public void Update()
+    {
+        AttachPoint.LookAt(OverrideAttachPointToTarget);
+    }
     public void ActivateEffect()
     {
 
-        if(MainEffect == null) return;
+        if (MainEffect == null) 
+            return;
+        
         GameObject instance;
-        if (OverrideAttachPointToTarget == null) instance = Instantiate(MainEffect, AttachPoint.transform.position, AttachPoint.transform.rotation);
-        else instance = Instantiate(MainEffect, AttachPoint.transform.position, Quaternion.LookRotation(-(AttachPoint.position - OverrideAttachPointToTarget.position)));
+        if (OverrideAttachPointToTarget == null)
+            instance = Instantiate(MainEffect, AttachPoint.transform.position, AttachPoint.transform.rotation);
+
+        else
+            instance = Instantiate(MainEffect, AttachPoint.transform.position, Quaternion.LookRotation(-(AttachPoint.position - OverrideAttachPointToTarget.position)));
+
+        instance.transform.position = AttachPoint.transform.position + AttachPoint.transform.forward * m_forwardOffset;
+
+        //if (OverrideAttachPointToTarget == null)
+        //    instance = Instantiate(MainEffect, pos, AttachPoint.rotation);
+
+        //else
+        //    instance = Instantiate(MainEffect, pos, Quaternion.LookRotation(-(pos - OverrideAttachPointToTarget.position)));
+
+
         UpdateEffectForMobileIsNeed(instance);
-        if (Effect_DestroyTime > 0.01f) Destroy(instance, Effect_DestroyTime);
+        
+        if (Effect_DestroyTime > 0.01f) 
+            Destroy(instance, Effect_DestroyTime);
     }
 
-    public void ActivateAdditionalEffect()
-    {
-        if (AdditionalEffect == null) return;
-        if (AdditionalEffectAttachPoint != null)
-        {
-            var instance = Instantiate(AdditionalEffect, AdditionalEffectAttachPoint.transform.position, AdditionalEffectAttachPoint.transform.rotation);
-            UpdateEffectForMobileIsNeed(instance);
-            if (AdditionalEffect_DestroyTime > 0.01f) Destroy(instance, AdditionalEffect_DestroyTime);
-        }
-        else AdditionalEffect.SetActive(true);
-    }
+    //public void ActivateAdditionalEffect()
+    //{
+    //    if (AdditionalEffect == null) return;
+    //    if (AdditionalEffectAttachPoint != null)
+    //    {
+    //        var instance = Instantiate(AdditionalEffect, AdditionalEffectAttachPoint.transform.position, AdditionalEffectAttachPoint.transform.rotation);
+    //        UpdateEffectForMobileIsNeed(instance);
+    //        if (AdditionalEffect_DestroyTime > 0.01f) Destroy(instance, AdditionalEffect_DestroyTime);
+    //    }
+    //    else AdditionalEffect.SetActive(true);
+    //}
 
     public void ActivateCharacterEffect()
     {
-        if (CharacterEffect == null) return;
+        if (CharacterEffect == null)
+            return;
+        
+        //var instance = Instantiate(CharacterEffect, pos, CharacterAttachPoint.transform.rotation, CharacterAttachPoint.transform);
         var instance = Instantiate(CharacterEffect, CharacterAttachPoint.transform.position, CharacterAttachPoint.transform.rotation, CharacterAttachPoint.transform);
+        
         UpdateEffectForMobileIsNeed(instance);
-        if (CharacterEffect_DestroyTime > 0.01f) Destroy(instance, CharacterEffect_DestroyTime);
+        
+        if (CharacterEffect_DestroyTime > 0.01f)
+            Destroy(instance, CharacterEffect_DestroyTime);
     }
 
-    public void ActivateCharacterEffect2()
-    {
-        if (CharacterEffect2 == null) return;
-        var instance = Instantiate(CharacterEffect2, CharacterAttachPoint2.transform.position, CharacterAttachPoint2.transform.rotation, CharacterAttachPoint2);
-        UpdateEffectForMobileIsNeed(instance);
-        if (CharacterEffect2_DestroyTime > 0.01f) Destroy(instance, CharacterEffect2_DestroyTime);
-    }
+    //public void ActivateCharacterEffect2()
+    //{
+    //    if (CharacterEffect2 == null) return;
+    //    var instance = Instantiate(CharacterEffect2, CharacterAttachPoint2.transform.position, CharacterAttachPoint2.transform.rotation, CharacterAttachPoint2);
+    //    UpdateEffectForMobileIsNeed(instance);
+    //    if (CharacterEffect2_DestroyTime > 0.01f) Destroy(instance, CharacterEffect2_DestroyTime);
+    //}
 
     void UpdateEffectForMobileIsNeed(GameObject instance)
     {
--- Assets/SpellVFX/SpellTesting.cs
+++ Assets/SpellVFX/SpellTesting.cs
@@ -6,11 +6,37 @@ public class SpellTesting : MonoBehaviour
 {
     [SerializeField]
     private RFX4_EffectEvent eventHandler;
-    
+
+    [Header("Debugging Options")]
+    [SerializeField]
+    private bool m_autoFire = false;
+
+    [SerializeField]
+    private float m_timeTillAutoFire;
+
+    private float m_counter = 0;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            eventHandler.ActivateEffect();
+            CastSpell();
+
+
+        if (m_autoFire)
+        {
+            m_counter += Time.deltaTime;
+
+            if(m_counter > m_timeTillAutoFire)
+            {
+                m_counter = 0;
+                CastSpell();
+            }
+        }
+    }
+
+    void CastSpell()
+    {
+        eventHandler.ActivateEffect();
+       // eventHandler.ActivateCharacterEffect();
     }
 }
