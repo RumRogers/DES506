8702c627951a6a033e63620e321e2c6f7891d21d
Michael Shaw <michaelshaw575@googlemail.com>
2020-07-28 10:55:26 +0100

    Respawn FMOD Events
    Just the actual files for then respawn stuff, no audio yet

---
+++ Assets/_Scripts/Audio/MenuSFXManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+
+public class MenuSFXManager : MonoBehaviour
+{
+    //Player Movement
+
+    public void playLeftstepSFX ()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PLAYER/MOVEMENT/Player_LeftStep");
+    }
+
+    public void playRightstepSFX()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PLAYER/MOVEMENT/Player_RightStep");
+    }
+
+    public void playJumpSFX()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PLAYER/MOVEMENT/Player_Jump");
+    }
+
+    public void playLandSFX()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PLAYER/MOVEMENT/Player_Land");
+    }
+
+    //Player Spellcasting
+    public void spellcastSFX()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SPELLS/Spell_Cast");
+    }
+}
