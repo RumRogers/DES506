3736627157abf88e84b76302d01a5ee46a8ed1ab
Jordan Walsh <jordanwalsh9519@gmail.com>
2020-08-03 16:50:58 +0100

    Re-did all the buttons to add the quill

---
+++ Assets/ButtonIconScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonIconScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    GameObject m_quill;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        m_quill.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        m_quill.SetActive(false);
+    }
+
+}
