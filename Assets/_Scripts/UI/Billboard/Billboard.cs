4b80436780e8280b54297b5c00ca705fb117d22a
Andrea Serreli <andreaserreli1984@gmail.com>
2020-08-03 18:30:44 +0200

    Speech Bubbles: Billboards effect + Icon change

---
+++ Assets/_Scripts/UI/Billboard/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameUI.Effects
+{
+    public class Billboard : MonoBehaviour
+    {
+        Transform m_cameraTransform;
+
+        void Start()
+        {
+            m_cameraTransform = Camera.main.transform;
+        }
+
+        void Update()
+        {
+            transform.rotation = Quaternion.LookRotation(-m_cameraTransform.forward, m_cameraTransform.up);
+        }
+    }
+}
---
+++ Assets/_Scripts/UI/Dialogue/SpeechBubbleChanger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbleChanger : MonoBehaviour
+{
+    [Header("Far and close sprites")]
+    [SerializeField]
+    Sprite m_farSprite;
+    [SerializeField]
+    Sprite m_closeSprite;
+    [Header("Threshold distance to change to bubble speech icon")]
+    [SerializeField]
+    float m_distanceThreshold = 2;
+
+    SpriteRenderer m_spriteRenderer;
+    Transform m_playerTransform;
+    float m_distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_playerTransform = GameObject.FindGameObjectWithTag("Player").transform;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Vector3.Distance(transform.position, m_playerTransform.position));
+        if(Vector3.Distance(transform.position, m_playerTransform.position) <= m_distanceThreshold)
+        {
+            m_spriteRenderer.sprite = m_closeSprite;
+        }
+        else
+        {
+            m_spriteRenderer.sprite = m_farSprite;
+        }
+    }
+}
